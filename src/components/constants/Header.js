c1b247c0514c13c2fd92b74820193b6f7129ee12
Michael DiPaolo <michael.dipaolo1@icloud.com>
2023-09-02 23:43:36 -0400

    Add toggle switch to Header & prettify

--- src/components/constants/Header.js
+++ src/components/constants/Header.js
@@ -1,14 +1,16 @@
-import React from "react";
-import { motion } from "framer-motion";
-import tw from "twin.macro";
-import styled from "styled-components";
-import { css } from "styled-components/macro"; //eslint-disable-line
+import React from 'react';
+import { motion } from 'framer-motion';
+import tw from 'twin.macro';
+import styled from 'styled-components';
+import { css } from 'styled-components/macro'; //eslint-disable-line
 
-import useAnimatedNavToggler from "../../helpers/useAnimatedNavToggler.js";
+import ToggleDevModeSwitch from '../misc/ToggleDevModeSwitch.js';
 
-import logo from "../../assets/logo-sig_solitude-bold.svg";
-import { ReactComponent as MenuIcon } from "feather-icons/dist/icons/menu.svg";
-import { ReactComponent as CloseIcon } from "feather-icons/dist/icons/x.svg";
+import useAnimatedNavToggler from '../../helpers/useAnimatedNavToggler.js';
+
+import logo from '../../assets/logo-sig_solitude-bold.svg';
+import { ReactComponent as MenuIcon } from 'feather-icons/dist/icons/menu.svg';
+import { ReactComponent as CloseIcon } from 'feather-icons/dist/icons/x.svg';
 
 const Header = tw.header`
   pb-0
@@ -31,11 +33,11 @@ export const PrimaryLink = tw(NavLink)`
 `;
 
 export const LogoLink = styled(NavLink)`
-  ${tw`flex items-center font-black border-b-0 text-3xl! ml-0!`};
+	${tw`flex items-center font-black border-b-0 text-3xl! ml-0!`};
 
-  img {
-    ${tw`w-20 mr-3 opacity-90 hocus:opacity-100`}
-  }
+	img {
+		${tw`w-20 mr-3 opacity-90 hocus:opacity-100`}
+	}
 `;
 
 export const MobileNavLinksContainer = tw.nav`flex flex-1 items-center justify-between`;
@@ -43,79 +45,114 @@ export const NavToggle = tw.button`
   lg:hidden z-20 mr-8 focus:outline-none hocus:text-go-500 transition duration-300
 `;
 export const MobileNavLinks = motion(styled.div`
-  ${tw`lg:hidden z-10 fixed top-0 inset-x-0 mx-4 mt-10 mb-4 p-4 border text-center rounded-lg text-holiness bg-rain`}
-  ${NavLinks} {
-    ${tw`flex flex-col items-center`}
-  }
+	${tw`lg:hidden z-10 fixed top-0 inset-x-0 mx-4 mt-10 mb-4 p-4 border text-center rounded-lg text-holiness bg-rain`}
+	${NavLinks} {
+		${tw`flex flex-col items-center`}
+	}
 `);
 
 export const DesktopNavLinks = tw.nav`
   hidden lg:flex flex-1 justify-between items-center
 `;
 
-export default ({ roundedHeaderButton = false, logoLink, links, className, collapseBreakpointClass = "lg" }) => {
-  const { showNavLinks, animation, toggleNavbar } = useAnimatedNavToggler();
-  const collapseBreakpointCss = collapseBreakPointCssMap[collapseBreakpointClass];
+export default ({
+	roundedHeaderButton = false,
+	logoLink,
+	links,
+	className,
+	collapseBreakpointClass = 'lg',
+}) => {
+	const { showNavLinks, animation, toggleNavbar } = useAnimatedNavToggler();
+	const collapseBreakpointCss = collapseBreakPointCssMap[collapseBreakpointClass];
 
-  const defaultLinks = [
-    <NavLinks key={1}>
-      <NavLink href="/#process" onClick={toggleNavbar}>Process</NavLink>
-      <NavLink href="/#options" onClick={toggleNavbar}>Options</NavLink>
-      <NavLink href="/#portfolio" onClick={toggleNavbar}>Portfolio</NavLink>
-      <NavLink href="/#testimonials" onClick={toggleNavbar}>Testimonials</NavLink>
-      <NavLink href="/#faq" onClick={toggleNavbar}>FAQ</NavLink>
-      <PrimaryLink css={roundedHeaderButton && tw`rounded-full`}href="/#contact" onClick={toggleNavbar}>Start</PrimaryLink>
-    </NavLinks>
-  ];
+	const defaultLinks = [
+		<NavLinks key={1}>
+			<NavLink href='/#process' onClick={toggleNavbar}>
+				Process
+			</NavLink>
+			<NavLink href='/#options' onClick={toggleNavbar}>
+				Options
+			</NavLink>
+			<NavLink href='/#portfolio' onClick={toggleNavbar}>
+				Portfolio
+			</NavLink>
+			<NavLink href='/#testimonials' onClick={toggleNavbar}>
+				Testimonials
+			</NavLink>
+			<NavLink href='/#faq' onClick={toggleNavbar}>
+				FAQ
+			</NavLink>
+			<PrimaryLink
+				css={roundedHeaderButton && tw`rounded-full`}
+				href='/#contact'
+				onClick={toggleNavbar}
+			>
+				Start
+			</PrimaryLink>
+		</NavLinks>,
+	];
 
-  const defaultLogoLink = (
-    <LogoLink href="/">
-      <img src={logo} alt="logo for MAD Web Design - signature spelling M.A.D." />
-    </LogoLink>
-  );
+	const defaultLogoLink = (
+		<LogoLink href='/'>
+			<img src={logo} alt='logo for MAD Web Design - signature spelling M.A.D.' />
+		</LogoLink>
+	);
 
-  logoLink = logoLink || defaultLogoLink;
-  links = links || defaultLinks;
+	const toggleDevMode = (
+		<ToggleDevModeSwitch />
+	);
 
-  return (
-    <Header className={className || "header-light"}>
-      <DesktopNavLinks css={collapseBreakpointCss.desktopNavLinks}>
-        {logoLink}
-        {links}
-      </DesktopNavLinks>
+	logoLink = logoLink || defaultLogoLink;
+	links = links || defaultLinks;
 
-      <MobileNavLinksContainer css={collapseBreakpointCss.mobileNavLinksContainer}>
-        {logoLink}
-        <MobileNavLinks initial={{ x: "150%", display: "none" }} animate={animation} css={collapseBreakpointCss.mobileNavLinks}>
-          {links}
-        </MobileNavLinks>
-        <NavToggle onClick={toggleNavbar} className={showNavLinks ? "open" : "closed"}>
-          {showNavLinks ? <CloseIcon tw="w-6 h-6 text-holiness" /> : <MenuIcon tw="w-6 h-6" />}
-        </NavToggle>
-      </MobileNavLinksContainer>
-    </Header>
-  );
+	return (
+		<Header className={className || 'header-light'}>
+			<DesktopNavLinks css={collapseBreakpointCss.desktopNavLinks}>
+				{logoLink}
+				{toggleDevMode}
+				{links}
+			</DesktopNavLinks>
+
+			<MobileNavLinksContainer css={collapseBreakpointCss.mobileNavLinksContainer}>
+				{logoLink}
+				<MobileNavLinks
+					initial={{ x: '150%', display: 'none' }}
+					animate={animation}
+					css={collapseBreakpointCss.mobileNavLinks}
+				>
+					{links}
+				</MobileNavLinks>
+				<NavToggle onClick={toggleNavbar} className={showNavLinks ? 'open' : 'closed'}>
+					{showNavLinks ? (
+						<CloseIcon tw='w-6 h-6 text-holiness' />
+					) : (
+						<MenuIcon tw='w-6 h-6' />
+					)}
+				</NavToggle>
+			</MobileNavLinksContainer>
+		</Header>
+	);
 };
 
 const collapseBreakPointCssMap = {
-  sm: {
-    mobileNavLinks: tw`sm:hidden`,
-    desktopNavLinks: tw`sm:flex`,
-    mobileNavLinksContainer: tw`sm:hidden`
-  },
-  md: {
-    mobileNavLinks: tw`md:hidden`,
-    desktopNavLinks: tw`md:flex`,
-    mobileNavLinksContainer: tw`md:hidden`
-  },
-  lg: {
-    mobileNavLinks: tw`lg:hidden`,
-    desktopNavLinks: tw`lg:flex`,
-    mobileNavLinksContainer: tw`lg:hidden`
-  },
-  xl: {
-    mobileNavLinks: tw`lg:hidden`,
-    desktopNavLinks: tw`lg:flex`,
-    mobileNavLinksContainer: tw`lg:hidden`
-  }
+	sm: {
+		mobileNavLinks: tw`sm:hidden`,
+		desktopNavLinks: tw`sm:flex`,
+		mobileNavLinksContainer: tw`sm:hidden`,
+	},
+	md: {
+		mobileNavLinks: tw`md:hidden`,
+		desktopNavLinks: tw`md:flex`,
+		mobileNavLinksContainer: tw`md:hidden`,
+	},
+	lg: {
+		mobileNavLinks: tw`lg:hidden`,
+		desktopNavLinks: tw`lg:flex`,
+		mobileNavLinksContainer: tw`lg:hidden`,
+	},
+	xl: {
+		mobileNavLinks: tw`lg:hidden`,
+		desktopNavLinks: tw`lg:flex`,
+		mobileNavLinksContainer: tw`lg:hidden`,
+	},
 };
